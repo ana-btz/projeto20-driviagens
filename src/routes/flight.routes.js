7614c5123b0b18e6f2993e61f7bdb41c7eaeb611
Ana Beatriz <silva.anabtz@gmail.com>
2023-12-18 09:35:41 -0300

    feat: GET /flights

--- src/routes/flight.routes.js
+++ src/routes/flight.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { flightSchema } from "../schemas/flight.schema.js";
+import { flightController } from "../controllers/flight.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 
 const flightRouter = Router();
 
-flightRouter.post(
-  "/flights",
-  validateSchema(flightSchema),
-  flightController.create
-);
+flightRouter
+  .post("/flights", validateSchema(flightSchema), flightController.create)
+  .get("/flights", flightController.findAll);
 
 export default flightRouter;
